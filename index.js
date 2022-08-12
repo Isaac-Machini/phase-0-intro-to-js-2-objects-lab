1543d16864a4f22a574a305527832cd73de077c9
Isaac-Machini <isaac.machini@student.moringaschool.com>
2022-08-12 08:49:10 +0300

    complete assignment'

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+const employee = {
+    name: "Bashaar",
+    street: "carnasie",
+}; 
+
+function updateEmployeeWithKeyAndValue(employee,key,value) {
+    const updatedEmployee={...employee};
+    updatedEmployee[key]=value;
+    return updatedEmployee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key]=value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const NewEmployee ={...employee};
+    delete NewEmployee[key];
+    return NewEmployee;
+    
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
